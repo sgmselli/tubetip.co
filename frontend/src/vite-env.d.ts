31cd9cc921b02624f97954f615edd7c805974b77
sgmselli <sellings.matthew@gmail.com>
2025-10-09 11:59:09 +0100

    Commit missed upload helper file

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -3,6 +3,8 @@ declare module "*.svg";
 declare module "*.jpeg";
 declare module "*.jpg";
 declare module "*.css";
+declare module "*.heic";
+declare module "*.heif";
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
